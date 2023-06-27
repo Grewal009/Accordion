b3516c3e8e16c52cbaee7b375a2ed647ce03425b
Aneel Grewal <aneel.grewal@hotmail.com>
2023-06-27 10:15:35 +0200

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-red-600 text-xl font-bold">Hello React</h1>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
